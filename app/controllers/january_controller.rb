c415e2d1a798be190904f5a83f9756b4384b0bc8
Dylan Ham <dylanham@gmail.com>
2015-01-28 21:58:20 -0700

    user can go to any month

---
+++ app/controllers/january_controller.rb
@@ -0,0 +1,38 @@
+class JanuaryController <ApplicationController
+  def month_one
+  end
+
+  def month_two
+  end
+
+  def month_three
+  end
+
+  def month_four
+  end
+
+  def month_five
+  end
+
+  def month_six
+  end
+
+  def month_seven
+  end
+
+  def month_eight
+  end
+
+  def month_nine
+  end
+
+  def month_ten
+  end
+
+  def month_eleven
+  end
+
+  def month_twelve
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,19 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'january#month_one'
+  get  'february/', to: 'january#month_two'
+  get  'march/', to: 'january#month_three'
+  get  'april/', to: 'january#month_four'
+  get  'may/', to: 'january#month_five'
+  get  'june/', to: 'january#month_six'
+  get  'july/', to: 'january#month_seven'
+  get  'august/', to: 'january#month_eight'
+  get  'september/', to: 'january#month_nine'
+  get  'october/', to: 'january#month_ten'
+  get  'november/', to: 'january#month_eleven'
+  get  'december/', to: 'january#month_twelve'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
